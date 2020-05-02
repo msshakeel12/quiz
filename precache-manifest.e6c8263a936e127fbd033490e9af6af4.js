cfebf205a2b60244daabd56d1637a4e485e64b6d
msshakeel12 <sadiqshakeel12@gmail.com>
2020-05-02 20:10:13 -0230

    Updates

---
+++ precache-manifest.e6c8263a936e127fbd033490e9af6af4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0ad0228faa8cfecc8ee72acb380f0e",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "c1385d8abfb510a98192",
+    "url": "/quiz/static/css/main.dc3390c5.chunk.css"
+  },
+  {
+    "revision": "911e3378e2ac6a451a55",
+    "url": "/quiz/static/js/2.29a53df4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz/static/js/2.29a53df4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1385d8abfb510a98192",
+    "url": "/quiz/static/js/main.303d76a3.chunk.js"
+  },
+  {
+    "revision": "7e5e903b84d3d404dac3",
+    "url": "/quiz/static/js/runtime-main.bf451133.js"
+  }
+]);
